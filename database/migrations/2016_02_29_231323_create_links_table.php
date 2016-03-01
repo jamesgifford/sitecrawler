3d3ea7b27f733b3ca32f295a8aa73b7a019c3eae
James Gifford <jg.webdeveloper@gmail.com>
2016-02-29 23:48:29 -0800

    Add table migrations

--- database/migrations/2016_02_29_231323_create_links_table.php
+++ database/migrations/2016_02_29_231323_create_links_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateLinksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,12 +12,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('links', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('site_id')->unsigned()->index();
+            $table->string('url');
+            $table->enum('type', ['page', 'file']);
+            $table->string('extension');
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('links');
     }
 }
