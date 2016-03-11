e69c90ce212eeb47167f79d5944499fe01a57206
James Gifford <jg.webdeveloper@gmail.com>
2016-03-11 14:07:56 -0800

    Re-add recent changes: add check for directories

--- config/crawl.php
+++ config/crawl.php
@@ -2,26 +2,19 @@
 
 return [
 
-// Extensions used by valid webpages (that might contain links to index)
-'pageExtensions' => [
-    'html', 'htm', 'xhtml', 
-    'php', 
-    'aspx', 'asp'
-],
-
 // File extensions to record (path will be recorded, but contents will not be indexed)
-'fileExtensions' => [
+'includeFileExtensions' => [
     'pdf', 'xls', 'rtf', 'txt', 'xml'
 ],
 
 // File extensions to skip completely (will not be recorded or searched for links)
-'excludeExtensions' => [
+'excludeFileExtensions' => [
     'css', 'ashx', 
     'js', 
     'gif', 'png', 'jpg', 'jpeg', 'bmp', 'tif', 'tiff', 'ico', 'ps', 
     'wav', 'mp3', 'au', 'aiff', 'mpg', 'mpeg', 'mov', 'qt', 'avi', 
     'zip', 'gz', 'tar', 'exe', 'tgz', 'bz', 'bz2', 'z', 'gzip', 'sit', 'jar', 
-    'vcf', 'arj', 'bin', 'ram', 'ra', 'arc', 'hqx', 'sea', 'uu', 'cl',  
+    'vcf', 'arj', 'bin', 'ram', 'ra', 'arc', 'hqx', 'sea', 'uu', 'cl', 
 ],
 
 ];
